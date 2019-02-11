d6c9bab253cbf9692a688e99c55240d63f220159
remind <remindbg@gmail.com>
2019-02-11 16:02:31 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
                 <div class="card-body">
                     This is a very simple Demo App showing the daily horoscope. The app uses Laravel as a backend and some VueJS components. However, since the backend does not care about saving any data, there is no real model. The app uses aztro.readthedocs.io as an API. The app is hosted in heroku free plan and the github is
-                    <a href="https://github.com/remindbg/HoroscopesVue">here</a>. 
+                    <a href="https://github.com/remindbg/HoroscopesVue">here</a>.
                     <p>Pick a Zodiac from the sidebar menu
                     </p>
                 </div>
